3b1485c09fe82902c797b6308e861336f9df3c9d
Oliver Eielson <47375940+olivereielson@users.noreply.github.com>
2022-10-23 15:23:31 -0400

    Test

--- check_out.js
+++ check_out.js
@@ -1,52 +1,57 @@
+
+//set up all the variables
 let cart;
 let folder_name = [];
 let food_name = [];
 let subtotal = 0;
 let tax = 0;
 let total = 0;
-
-
 let first_name,last_name,card_number,card_code,card_date;
 
 
 
-
+//capitalize the food names
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+//checks for an error in the form.
 function check_error(bool, field){
     
     if(bool){
+        //if no error make boarder normal
         $(field).css('border', '2px solid rgb(177, 214, 190)');
         return 0;
     }else{
+        //if error make boarder red
         $(field).css('border', '2px solid red');
         return 1;
     }
 
 }
 
-
+//validate the form
 function validate(){
     
     let error=0;
-    
+    //check all inputs
     error+=check_error(first_name!=null,"#first_name");
     error+=check_error(last_name!=null,"#last_name");
     error+=check_error((card_code!=null && card_code.length===3),"#card_code");
     error+=check_error((card_number!=null && card_number.length===16),"#card_number");
     error+=check_error(card_date!=null,"#card_date");
-    
+
+    // if no errors then move on.
     if(error===0){
         alert("thanks for your purchase ");
     }
 }
 
 
-
+//make all the input listeners
 function make_input_listeners(){
 
+    // this is all self-explanatory
     $("#first_name").change(function (){
         first_name=this.value;
     });
@@ -63,22 +68,14 @@ function make_input_listeners(){
         card_number=this.value;
     });
     $("#buy_now").click(function () {
-       
         validate();
-        
     });
     
 }
 
 
-
-
-
-$(document).ready(async function () {
-
-    cart = JSON.parse(localStorage.cart);
-    console.log(cart);
-
+async function get_food_data() {
+    //async function to get food names from file
     await $.get('/food.txt', function (data) {
         let lines = data.split('\n');
         for (let i = 2; i < lines.length; i++) {
@@ -87,13 +84,27 @@ $(document).ready(async function () {
             food_name.push(word[0]);
         }
     });
+}
 
 
+
+$(document).ready(async function () {
+
+    // parse the json in local storage to get the cart values back
+    cart = JSON.parse(localStorage.cart);
+    console.log(cart);
+
+    await get_food_data();
+
+
+    //go through all the items and add them to order summery
     for (let i = 0; i < 36; i++) {
 
+        //if not ordered then hide it
         if (cart[i] === 0) {
             $("#line" + i.toString()).hide();
         } else {
+            //if ordered calculate price and update it on page
             let price = cart[i] * 4.20;
             subtotal += price;
             $("#quantity" + i.toString()).text("x" + cart[i].toString());
@@ -103,12 +114,14 @@ $(document).ready(async function () {
         }
     }
 
+    //update the totals,tax,subtotals
     $("#subtotal").text("$" + subtotal.toFixed(2));
     tax = (subtotal * 0.0625);
     $("#tax").text("$" + tax.toFixed(2));
     total = subtotal + tax;
     $("#total").text("$" + total.toFixed(2));
 
+    //make listeners
     make_input_listeners();
 
 
--- product.js
+++ product.js
@@ -1,12 +1,15 @@
+
+//set up var names
 let folder_name = [];
 let food_name = [];
 let tt = [];
 let cart = [];
 
+//function to capitalize first letter
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
+//function to calculate total
 function total(){
     let total=0;
     for(let  i=0; i<36;i++){
@@ -16,8 +19,8 @@ function total(){
     
 }
 
-$(document).ready(async function () {
-
+async function get_food_data() {
+    //async function to get food names from file
     await $.get('/food.txt', function (data) {
         let lines = data.split('\n');
         for (let i = 2; i < lines.length; i++) {
@@ -26,24 +29,33 @@ $(document).ready(async function () {
             food_name.push(word[0]);
         }
     });
-    
+}
+
+$(document).ready(async function () {
+
+
+   await get_food_data();
+
+   //add listener to cart
     $("span[class=cart]").click(function (){
         window.location.href = "check_out.html";
         localStorage.setItem("cart",JSON.stringify(cart));
     });
 
+    //set up everything for all the foods
     for (let i = 0; i < 36; i++) {
         cart.push(0);
+        //set up the add the cart buttons
         $("#minus" + i.toString()).hide();
         $("#plus" + i.toString()).hide();
         $("#number" + i.toString()).hide();
 
+        //update the name, price, image and buttons
         $("h3[id=Title" + i.toString() + "]").text(capitalizeFirstLetter(food_name[i]));
         $("#price" + i.toString()).text("$4.20");
         $("#img" + i.toString()).attr("src", folder_name[i]);
         $("#button" + i.toString()).click(function () {
-
-
+            //show the numbers on the cart buttons when clicked.
             cart[i]++;
             $("#cart").text(total());
             $("#button" + i.toString()).hide();
@@ -51,27 +63,28 @@ $(document).ready(async function () {
             $("#plus" + i.toString()).fadeIn();
             $("#number" + i.toString()).show().text(cart[i].toString());
 
-
-
         });
 
+        //add plus button listener
         $("#plus" + i.toString()).click(function () {
             cart[i]++;
             $("#cart").text(total());
             $("#number" + i.toString()).show().text(cart[i].toString());
 
         });
+        //add minus button listener
         $("#minus" + i.toString()).click(async function () {
+            //update the cart
             cart[i]--;
             $("#cart").text(total());
-
+            //chnage the cart number
             $("#number" + i.toString()).show().text(cart[i].toString());
 
+            //if everything is removed then hide numbers and plus/minus
             if (cart[i] <= 0) {
                 await $("#minus" + i.toString()).hide();
                 await $("#plus" + i.toString()).hide();
                 await $("#number" + i.toString()).hide();
-
                 $("#button" + i.toString()).fadeIn();
 
             }
